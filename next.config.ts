4144e2dd2f0e231291136f0cb747caf22aeee447
Amana <amana@example.com>
2026-05-26 11:10:53 +0545

    Allow local network mobile device testing in nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: ['192.168.86.10', 'localhost:3000', 'localhost:3001']
+  }
 };
 
 export default nextConfig;
